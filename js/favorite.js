a2b0dfdbee9ef51e4967a1128d14ed0a5ea36e75
SheikhMujib <mbasheikhmujib@gmail.com>
2022-08-23 01:18:18 +0600

    Favorite Five section made with javaScript.

---
+++ js/favorite.js
@@ -0,0 +1,20 @@
+function addToFavorite(element) {
+    const favoritePlayer = element.parentNode.children[0].innerText;
+
+    const favoriteFootballers = document.getElementById('favorite-five');
+
+    const li = document.createElement('li');
+    li.innerText = favoritePlayer;
+
+    // favoriteFootballers.appendChild(li);
+
+    if (favoriteFootballers.childElementCount <= 4) {
+        favoriteFootballers.appendChild(li);
+    }
+    else {
+        alert('Hey! You cannot add more than 5 players.')
+        return;
+    }
+
+    element.setAttribute("disabled", "");
+}
